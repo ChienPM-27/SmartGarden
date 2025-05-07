afe3974965a85b0f6706ddc9e8cc4bb12f927a20
ChienPM-27 <pminhchien2006@gmail.com>
2025-05-07 22:38:15 +0700

    Home Tab

--- app/index.tsx
+++ app/index.tsx
@@ -6,13 +6,14 @@ import {
     ImageBackground,
 } from 'react-native';
 import { useRouter } from 'expo-router';
-import { images } from '@/constants/images'; // đảm bảo đã khai báo images.bg
+import { images } from '@/constants/images';
+import { StatusBar } from 'expo-status-bar';
 
 export default function IntroScreen() {
     const router = useRouter();
 
     const handleStart = () => {
-        router.replace('/(tabs)/login'); // chuyển sang màn hình login
+        router.replace('/(tabs)/login');
     };
 
     return (
@@ -21,32 +22,27 @@ export default function IntroScreen() {
             className="flex-1 justify-center items-center px-6"
             resizeMode="cover"
         >
-            <View
-                style={{
-                    backgroundColor: 'rgba(255, 255, 255, 0.85)',
-                    borderRadius: 16,
-                    padding: 32,
-                    alignItems: 'center',
-                    maxWidth: 360,
-                    width: '100%',
-                }}
-            >
+            <View className="bg-white/85 rounded-2xl p-8 items-center w-full max-w-[360px]">
+                <StatusBar style="dark" />
                 <Text className="text-4xl font-bold text-green-800 mb-4">
                     SmartGarden
                 </Text>
-                <Text className="text-lg text-gray-700 mb-6 text-center">
-                    Welcome to your smart gardening assistant.
+                <Text className="text-base text-gray-700 mb-6 text-center leading-relaxed">
+                    AI-powered gardening app that helps you manage plants,
+                    explore knowledge, get farming news & track your growth.
                 </Text>
 
                 <TouchableOpacity
                     onPress={handleStart}
                     className="bg-yellow-400 px-6 py-3 rounded-xl"
                 >
-                    <Text className="text-green-900 font-semibold text-lg">Get Started</Text>
+                    <Text className="text-green-900 font-semibold text-lg">
+                        Get Started
+                    </Text>
                 </TouchableOpacity>
             </View>
 
-            <Text className="text-green-950 font-semibold mt-3 text-sm opacity-80">
+            <Text className="text-green-950 font-semibold mt-2 text-sm opacity-80">
                 ©2025 SmartGarden. Grow smart, live green.
             </Text>
         </ImageBackground>
